7f314fdb164d826dfb8dcdf51dde40668330d327
0livierNadeau <olnad12@ulaval.ca>
2024-04-10 12:20:01 -0400

    Quixo Error

--- quixo_error.py
+++ quixo_error.py
@@ -3,3 +3,7 @@
 Classes:
     * QuixoError - Classe d'erreur pour le jeu Quixo.
 """
+class QuixoError(Exception):
+    def __init__(self, message, position=None):
+        super().__init__(message)
+        self.position = position
