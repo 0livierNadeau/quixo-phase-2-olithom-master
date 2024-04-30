75e33bcbef3b41c04286b0b03114b6a3f0d799b8
Thomas <thdul4@ulaval.ca>
2024-04-30 16:49:59 -0400

    Ajout du fichier quixo ai et creation de la class quixo ai.

---
+++ quixo_ia.py
@@ -0,0 +1,27 @@
+from quixo import Quixo
+
+
+class QuixoIA(Quixo):
+    def __init__(self):
+        pass
+
+    def valider_diagonale(self, plateau, nombre_pions):
+        pass
+
+    def valider_horizontale(self, plateau, nombre_pions):
+        pass
+
+    def valider_verticale(self, plateau, nombre_pions):
+        pass
+
+    def partie_terminée(self):
+        pass
+
+    def trouver_coup_gagnant(self, joueur):
+        pass
+
+    def trouver_coup_bloquant(self, joueur):
+        pass
+
+    def jouer_le_coup(self, joueur):
+        pass
